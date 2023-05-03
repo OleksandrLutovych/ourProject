c76b0976c137db56ab3b7341e6a6e3fd295024de
NIKNIK67 <1805niknik67@gmail.com>
2023-05-03 10:55:52 +0200

    Authentication and Authorization

--- Server/Controllers/Api.cs
+++ Server/Controllers/Api.cs
@@ -2,15 +2,48 @@
 using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using System.Text;
-using System.Linq;
 using System.Net;
 using Microsoft.AspNetCore.Cors;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using Server.Migrations;
+using Server;
 
 namespace Server.Controllers
 {
     [DisableCors]
     public class Api:ControllerBase
     {
+        #region Private
+        private string EncryptPassword(string password) => BitConverter.ToString(SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(password))).Replace("-", "").ToLower();
+        #endregion
+        #region Routes
+        [HttpGet]
+        public IResult Login(User user)
+        {
+            JwtSecurityToken token = new JwtSecurityToken();
+            List<Claim> claims = new List<Claim>();
+            User resultUser = new User();
+            using(MySQLDbContext db = new MySQLDbContext())
+            {
+                if (db.Users.Where(x => x.Email == user.Email && x.Password == EncryptPassword(user.Password)).Any())
+                {
+                    token = new JwtSecurityToken(
+                        issuer: Auth.Issuer,
+                        audience: Auth.Audience,
+                        claims: claims,
+                        expires: DateTime.UtcNow.Add(TimeSpan.FromDays(7)),
+                        signingCredentials: new Microsoft.IdentityModel.Tokens.SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Auth.Key)),SecurityAlgorithms.HmacSha256)
+                    );
+                    resultUser = db.Users.Where(x => x.Email == user.Email && x.Password == EncryptPassword(user.Password)).First();
+
+
+                }
+            }
+            var response = new { resultUser.Id, resultUser.Name, resultUser.LastName, resultUser.Email, resultUser.Role, resultUser.CreationDate, token };
+            return Results.Json(response);
+        }
         [HttpGet]
         public List<User> GetAllUsers()
         {
@@ -27,14 +60,14 @@ namespace Server.Controllers
                 return db.Users.ToList().Select(x => { x.Password = ""; return x; }).Where(x=>x.Id==id)?.First(); 
             }
         }
+        
         [HttpPost]
         public HttpResponseMessage CreateNewUser(User user)
         {
             if(user.Password is not null && user.Email is not null && user.Name is not null && user.LastName is not null)
             using (MySQLDbContext db = new MySQLDbContext())
             {
-                byte[] bytes = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(user.Password));
-                user.Password = BitConverter.ToString(bytes).Replace("-", "").ToLower();
+                user.Password = EncryptPassword(user.Password);
                 user.CreationDate = DateTime.Now;
                 user.Role = Roles.User;
                 db.Users.Add(user);
@@ -48,5 +81,6 @@ namespace Server.Controllers
                 return new HttpResponseMessage(HttpStatusCode.BadRequest);
             }
         }
+        #endregion
     }
 }
--- Server/Server.cs
+++ Server/Server.cs
@@ -1,12 +1,30 @@
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace Server
 {
-
+    public static class Auth
+    {
+        public static string Issuer = "AuthServer";
+        public static string Audience = "AuthTokenUser";
+        public static string Key = "EncodedToken1";
+    }
     public class Server
     {
         public static void Main(params string[] args)
         {
             var builder = WebApplication.CreateBuilder();
             builder.Services.AddControllers();
+            builder.Services.AddAuthentication("Bearer").AddJwtBearer(options => options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters {
+                ValidateIssuer = true,
+                ValidIssuer = Auth.Issuer,
+                ValidateAudience = true,
+                ValidAudience = Auth.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Auth.Key))
+        });
+            builder.Services.AddAuthorization();
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy(name: "_AllowedOrigins",
@@ -20,11 +38,12 @@ namespace Server
                     });
             });
             var app = builder.Build();
+            app.UseAuthentication();
             app.UseRouting();
             app.UseHttpsRedirection();
-            app.UseAuthorization();
             app.UseStaticFiles();
             app.UseCors();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
               endpoints.MapControllerRoute(name:"default",pattern:"{controller}/{action}/{id?}").RequireCors("_AllowedOrigins");
